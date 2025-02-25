fda7d4fe924c0ec6a5a0fd3d438b48a6fd2c499b
Fernando Navarro <fernandoj.navarro@hotmail.com>
2025-02-25 19:44:10 -0300

    Initial commit

---
+++ packages/comments_repository/lib/src/comments_repository.dart
@@ -0,0 +1,7 @@
+/// {@template comments_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class CommentsRepository {
+  /// {@macro comments_repository}
+  const CommentsRepository();
+}
